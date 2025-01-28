a428eecf090f7bab9fb4436d14a40bf7c051f32b
Elliott <sendtoelle@hotmail.com>
2025-01-28 19:46:48 +0100

    fixade alla funktioner i JS

--- src/main/resources/static/script/fetch.mjs
+++ src/main/resources/static/script/fetch.mjs
@@ -7,25 +7,53 @@ export function fetchMeals(){
 }
 
 export function findMeal(id){
-    fetch("http://localhost:8080/find-meal")
+fetch(`http://localhost:8080/find-meal?id=${id}`, {
+        method: "GET",
+        headers: {
+        "Content-type": "application/json"
+        },
+    })
     .then(res => res.json())
+    .then(data => console.log(data));
+    console.log("Här är id:t jag sökte på:");
+    
+}
+
+export function addMeal(id, source, comment, favorite){
+    fetch("http://localhost:8080/add-meal", {
+        method: "POST",
+        body: JSON.stringify({
+            id: id,
+            source: source,
+            comment: comment,
+            favorite: favorite
+        }),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then(res => res.text())
     .then(data => console.log(data));
 }
 
-export function addMeal(){
-    fetch("http://localhost:8080/add-meal")
-    .then(res => res.json())
+export function deleteMeal(id){
+    fetch(`http://localhost:8080/delete-meal?id=${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then(res => res.text())
     .then(data => console.log(data));
 }
 
-export function deleteMeal(){
-    fetch("http://localhost:8080/delete-meal")
-    .then(res => res.json())
-    .then(data => console.log(data));
-}
-
-export function favoriteMeal(){
-    fetch("http://localhost:8080/favorite")
-    .then(res => res.json())
+export function favoriteMeal(id){
+    fetch(`http://localhost:8080/favorite?id=${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then(res => res.text())
     .then(data => console.log(data));
 }
--- src/main/resources/static/script/script.mjs
+++ src/main/resources/static/script/script.mjs
@@ -1,6 +1,11 @@
-import { fetchMeals, findMeal } from "./fetch.mjs";
+import { addMeal, deleteMeal, favoriteMeal, fetchMeals, findMeal } from "./fetch.mjs";
 
 fetchMeals();
-findMeal(5);
+findMeal(1);
 
+//alla funktioner nedan fungerar, disabled för att slippa ändra saker varje gång jag ändrar sidan.
+
+//addMeal(6, "frontendtest", "frontendtest", "false");
+//deleteMeal(6);
+//favoriteMeal(1);
 
